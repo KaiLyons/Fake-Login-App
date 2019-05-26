3088008a54849424d6b5df8bbe6183040b56abac
Jack Links Moisture Packets <50976878+SafyreLyons@users.noreply.github.com>
2019-05-26 08:38:42 -0600

    Made it look nicer in terminal and code
    no more of that endl, it just was too long tbh with `<< endl;` so I just decided to remove it. It also helped make it look nicer in the terminal

--- app.cpp
+++ app.cpp
@@ -7,22 +7,22 @@ int main(){
     string pin;
     string pinCheck;
 
-    cout << "enter username\n" << endl;
+    cout << "enter username\n";
     getline(cin, username);
 
-    cout << username << ", please enter your new Password!\n" << endl;
+    cout << username << ", please enter your new Password!\n";
     getline(cin, pin);
 
     system("clear");
 
-    cout << username << ", Please login with your password\n" << endl;
-    cout << "Password" << endl;
+    cout << username << ", Please login with your password\n";
+    cout << "Password : ";
     getline(cin, pinCheck);
 
     if (pin != pinCheck){
-        cout << "validation error" << endl;
+        cout << "validation error\n";
     } else {
-        cout << "validation success" << endl;
+        cout << "validation success\n";
     }
 
     return 0;
